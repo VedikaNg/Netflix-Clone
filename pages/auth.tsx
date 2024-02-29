a6b3f39bd4f12bde6f8bdf8993c44e67dbb6676e
vedika1907 <vedika.nigam@univest.in>
2024-02-29 22:37:01 +0530

    Update auth.tsx

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,5 @@
 import { useCallback,useState } from "react";
-import Input from "@/components/Input";
+import Input from "@/components/input";
 
 const Auth=() => {
   const[email,setEmail]=useState('');
