2a3650d2ecbcbf7adcf9acb4e6f9c2d286aa4817
aartii-s <aartisheth71@gmail.com>
2024-03-01 16:07:24 +0530

    AccountMenu, MobileMenu, Navbar, NavbarItem - new files added and index is modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 
 
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
-
+import { getSession } from "next-auth/react";
+import Navbar from "@/components/Navbar";
 export async function getServerSideProps(context: NextPageContext){
   const session = await getSession(context);
 
@@ -26,9 +26,7 @@ export default function Home() {
 
   return (
     <>
-    <h1 className="text-4xl text-green-500">Netflix Clone</h1>
-    <p className="text-white">Logged in as : {user?.name}</p>
-    <button className="h-10 w-full bg-white" onClick={()=> signOut()}>Logout!</button>
+      <Navbar/>
     </>
   )
 }
