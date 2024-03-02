e95a22d4fd37aa804df4ed7caa1ee31939380349
vedika1907 <vedika.nigam@univest.in>
2024-03-02 17:12:13 +0530

    Favorite

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,6 +1,8 @@
 import useBillboard from "@/hooks/useBillboard";
 import React from "react";
 
+import { AiOutlineInfoCircle } from 'react-icons/ai';
+
 const Billboard = () => {
     const { data } = useBillboard();
     return (
@@ -14,9 +16,9 @@ const Billboard = () => {
                 autoPlay
                 muted
                 loop
-                poster={data?.thumbnailurl}            
+                poster={data?.thumbnailUrl}            
             
-             src="{data?.videoUrl">
+             src={data?.videoUral}>
 
             </video>
             <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
@@ -31,7 +33,43 @@ const Billboard = () => {
                        ">
                     {data?.title}
                 </p>
+                <p className="
+                  text-white
+                  text-[8px]
+                  md:text-lg
+                  mt-3
+                  md:mt-8
+                  w-[90%]
+                  md:w-[80%]
+                  lg:w-[50%]
+                  drop-shadow-xl
 
+                ">
+                    {data?.description}
+                </p>
+                <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+                    <button 
+                    className="
+                       bg-white
+                       text-white
+                       bg-opacity-30
+                       rounded-md
+                       py-1 md:py-2
+                       px-2 md:px-4
+                       w-auto
+                       text-xs lg:text-lg
+                       font-semibold
+                       flex
+                       flex-row
+                       items-center
+                       hover:bg-opacity-20
+                       transition
+                    "
+                    >
+                        <AiOutlineInfoCircle className="mr-1"/>
+                        More Info
+                    </button>
+                </div>
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
+import useMovieList from "@/hooks/useMovieList";
 import useCurrentUser from "@/hooks/useCurrentUser";
-
 
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useFavorites from "@/hooks/useFavorites";
 
 export async function getServerSideProps(context: NextPageContext){
   const session = await getSession(context);
@@ -26,11 +28,18 @@ export async function getServerSideProps(context: NextPageContext){
 
 export default function Home() {
   const { data: user }= useCurrentUser();
+  const { data: movies = [] }= useMovieList();
+  const { data: favorites = []} = useFavorites();
 
   return (
     <>
       <Navbar/>
       <Billboard/>
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies}/>
+        <MovieList title="My List" data={favorites}/>
+      
+      </div>
     </>
   )
 }
